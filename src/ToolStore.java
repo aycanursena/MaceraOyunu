b5d2083b05f1f4ef4ae148ee7f6335a25b5ee62b
Ayça Nursena Korkmaz <aycanursenakorkmaz@gmail.com>
2023-02-08 22:47:44 +0300

    Armor sınıfı eklendi

--- src/ToolStore.java
+++ src/ToolStore.java
@@ -21,9 +21,11 @@ public class ToolStore extends NormalLoc{
         switch (selectCase){
             case 1:
                 printWeapon();
+                buyWeapon();
                 break;
             case 2:
                 printArmor();
+                buyArmor();
                 break;
             case 3:
                 System.out.println("Bir daha bekleriz !");
@@ -36,6 +38,13 @@ public class ToolStore extends NormalLoc{
         for (Weapon w : Weapon.weapons()){
             System.out.println(w.getId() + "- " + w.getName() + " < Para : " + w.getPrice() +
                     ", Hasar : " + w.getDamage() + " >");
+        }
+    }
+    public void printArmor(){
+        System.out.println("----- Zırhlar -----");
+        for (Armor ar : Armor.armors()){
+            System.out.println(ar.getId() + "- " + ar.getName() + " < Para : " + ar.getPrice() +
+                    ", Zırh : " + ar.getBlock() + " >");
         }
     }
 
@@ -64,8 +73,30 @@ public class ToolStore extends NormalLoc{
         }
     }
 
-    public void printArmor(){
-        System.out.println("Silahlar");
+    public void buyArmor(){
+        System.out.print("Bir zırh seçiniz : ");
+        int selectArmorId = input.nextInt();
+
+        while (selectArmorId < 1 || Armor.armors().length > 3){
+            System.out.print("Geçersiz değer, tekrar giriniz : ");
+            selectArmorId = input.nextInt();
+        }
+        Armor selectedArmor = Armor.getArmorObjByID(selectArmorId);
+        if(selectedArmor != null){
+            if(selectedArmor.getPrice() > this.getPlayer().getMoney()){
+                System.out.println("Yeterli paranız bulunmamaktadır...");
+            }else {
+                System.out.println(selectedArmor.getName() + " zırhını satın aldınız.");
+                int balance = this.getPlayer().getMoney() - selectedArmor.getPrice();
+
+                this.getPlayer().setMoney(balance);
+                System.out.println("Kalan paranız : " + this.getPlayer().getMoney());
+                System.out.println("Önceki silahınız : " + this.getPlayer().getInventory().getWeapon().getName());
+                this.getPlayer().getInventory().setArmor(selectedArmor);
+                System.out.println("Yeni silahınız : " + this.getPlayer().getInventory().getWeapon().getName());
+            }
+        }
+
     }
 }
 
